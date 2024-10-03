adecd979af622ee1501dc91789ae197a88a1e0c4
jaozin-10 <joao.felipe99@icloud.com>
2024-10-02 21:38:27 -0300

    Opção de saida
    Incluso no sistema a opção de saída para o usuário

--- Cadastro Ebac.c
+++ Cadastro Ebac.c
@@ -129,6 +129,7 @@ int main()
 	    printf("\t1 - Registrar nomes\n");
 	    printf("\t2 - Consultar nomes nomes\n");
 	    printf("\t3 - Deletar nomes nomes\n\n\n");
+	    printf("\t4 - Sair do sistema\n\n");
 	    printf( "Opção: "); // Fim do menu
 	
         scanf("%d", &opcao);  // armazenando a escolha do usuario 
@@ -148,7 +149,12 @@ int main()
     	        case 3:
     	        deletar(); // Chamada de funções 
 		        break;
-		  			   		
+		        case 4:
+		        printf("Obrigado por ultilizar o sistema!\n");
+		  		return 0;
+		  		break;
+				  
+				  	   		
 		        default:
 		        printf("Essa opção não esta disponivel!\n\n"); // Mensagem ao usuário 
 		        system("pause");
